09dd6cfab42519814b1d915df9f08a2a0a7fde40
Ravi Tiwari <ravitiwari254@gmail.com>
2023-06-22 20:14:07 +0530

    React Learning

--- src/index.js
+++ src/index.js
@@ -1,17 +1,62 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom';
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// JSX and Expression
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// ReactDOM.render(<h1> Hello World!! </h1>, document.getElementById('root'));
+
+// ReactDOM.render(React.createElement ("h1", null, "Hello User"), document.getElementById('root'));
+
+// ReactDOM.render(
+// <div>
+// <h2> Hello React</h2>
+// <p>Some Changes</p>
+// </div>,
+//  document.getElementById("root"))
+
+
+// ReactDOM.render(
+//   [
+//   <h2> Hello React</h2>,
+//   <p>How to use another type</p>],
+  
+//    document.getElementById("root"))
+
+ const fname = "Deepak";
+const lname = "Tiwari";
+const curDate = new Date().toLocaleDateString();
+const curTime = new Date().toLocaleTimeString();
+const img1 = "https://picsum.photos/200/300";
+const img2 = "https://picsum.photos/250/300";
+const img3 = "https://picsum.photos/300/300";
+const links = "https://www.google.co.in/";
+
+// ReactDOM.render(
+//    <>
+//    <h5>My Name is {fname + "  " + lname}</h5>
+//    <p>The Sum of Number is {6+6}</p>
+//    <p>The Rendom Number is {Math.random()}</p>
+//    </>,
+//    document.getElementById("root")
+// );
+
+ReactDOM.render(
+     <>
+     <h5 className='heading'> My Name is {fname + "  " + lname}</h5>
+     <div className='img-div'>
+     <img src={img1} alt="rendomImage"/>
+     <img src={img2} alt="rendomImage"/>
+     <a href={links} target='google'> 
+      <img src={img3} alt="rendomImage"/>
+     </a>
+     </div>
+     <p className='date'>Current Date is = {curDate}</p>
+     <p className='time'>Current Time is = {curTime}</p>
+
+     </>,
+     document.getElementById("root")
+  );
+
+
+
